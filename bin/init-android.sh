d0b7697693b0af2b043b3ca4ebf2079929bb8d0d
pulsation <philippe.sam-long@laposte.net>
2012-10-02 20:33:55 +0200

    separate init script

---
+++ bin/init-android.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+if [ ! `which wget` ]; then
+    echo "wget is needed to proceed." >&2;
+    exit 1
+fi
+
+if [ ! `which npm` ]; then
+    echo "npm is needed to proceed." >&2;
+    exit 1
+fi
+
+if [ ! `which android` ]; then
+    echo "android is needed to proceed." >&2;
+    exit 1
+fi
+
+if [ ! `which ant` ]; then
+    echo "ant is needed to proceed." >&2;
+    exit 1
+fi
+
+source bin/init.sh
+
+# fetch apache common codecs
+
+mkdir -p ${COMMON_CODEC_DIR}
+while [ "$(md5sum ${COMMON_CODEC_DIR}/${COMMON_CODEC_ARCHIVE} 2> /dev/null \
+        |cut -d ' ' -f 1)" != "${COMMON_CODEC_MD5}" ]; do
+    rm -f ${COMMON_CODEC_DIR}/${COMMON_CODEC_ARCHIVE}
+    (cd ${COMMON_CODEC_DIR} && wget -q ${COMMON_CODEC_URL})
+done
+
+# untar it
+(cd ${COMMON_CODEC_DIR} && tar xzf ${COMMON_CODEC_ARCHIVE})
+
+# copy it to corodva
+(mkdir -p ${CORDOVA_ANDROID_DIR}/framework/libs/ && \
+cp ${COMMON_CODEC_DIR}/commons-codec-${COMMON_CODEC_VERSION}/commons-codec-${COMMON_CODEC_VERSION}.jar \
+${CORDOVA_ANDROID_DIR}/framework/libs/)
+
+# Build the phonegap jar lib
+(cd ${CORDOVA_ANDROID_DIR}/framework && \
+    android update project -p . -t android-${CORDOVA_ANDROID_VERSION} && ant jar)
+
+# Install dependancies
+(cd ${CORDOVA_ANDROID_DIR}/bin && \
+    npm install)
+
+# create cordova android project
+(cd ${CORDOVA_ANDROID_DIR} && \
+    ./bin/create ../../../${ANDROID_PROJECT_DIR})
+
--- bin/init.sh
+++ bin/init.sh
@@ -10,26 +10,6 @@ if [ ! `which git` ]; then
     exit 1
 fi
 
-if [ ! `which wget` ]; then
-    echo "wget is needed to proceed." >&2;
-    exit 1
-fi
-
-if [ ! `which npm` ]; then
-    echo "npm is needed to proceed." >&2;
-    exit 1
-fi
-
-if [ ! `which android` ]; then
-    echo "android is needed to proceed." >&2;
-    exit 1
-fi
-
-if [ ! `which ant` ]; then
-    echo "ant is needed to proceed." >&2;
-    exit 1
-fi
-
 # checkout html5 project 
 if [ "${HTML5_PROJECT_SCM}" = "git" ]; then
     (cd projects/                                                           && \
@@ -41,33 +21,4 @@ fi
 
 # initialize git submodules
 git submodule update --init
-
-# fetch apache common codecs
-
-mkdir -p ${COMMON_CODEC_DIR}
-while [ "$(md5sum ${COMMON_CODEC_DIR}/${COMMON_CODEC_ARCHIVE} 2> /dev/null \
-        |cut -d ' ' -f 1)" != "${COMMON_CODEC_MD5}" ]; do
-    rm -f ${COMMON_CODEC_DIR}/${COMMON_CODEC_ARCHIVE}
-    (cd ${COMMON_CODEC_DIR} && wget -q ${COMMON_CODEC_URL})
-done
-
-# untar it
-(cd ${COMMON_CODEC_DIR} && tar xzf ${COMMON_CODEC_ARCHIVE})
-
-# copy it to corodva
-(mkdir -p ${CORDOVA_ANDROID_DIR}/framework/libs/ && \
-cp ${COMMON_CODEC_DIR}/commons-codec-${COMMON_CODEC_VERSION}/commons-codec-${COMMON_CODEC_VERSION}.jar \
-${CORDOVA_ANDROID_DIR}/framework/libs/)
-
-# Build the phonegap jar lib
-(cd ${CORDOVA_ANDROID_DIR}/framework && \
-    android update project -p . -t android-${CORDOVA_ANDROID_VERSION} && ant jar)
-
-# Install dependancies
-(cd ${CORDOVA_ANDROID_DIR}/bin && \
-    npm install)
-
-# create cordova android project
-(cd ${CORDOVA_ANDROID_DIR} && \
-    ./bin/create ../../../${ANDROID_PROJECT_DIR})
 
