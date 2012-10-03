4a4350434a430889b313273cfeab1a764b70c8c2
Philippe <psamlong@laposte.net>
2012-10-03 13:30:13 +0200

    only deploy one html5 project.

--- bin/init.sh
+++ bin/init.sh
@@ -4,10 +4,8 @@
 source config/config.sh
 
 # Initialize HTML5 projects
-for html5_framework in ${HTML5_FRAMEWORKS}; do
-    echo "Initializing ${html5_framework} project."
-    source bin/init-html5-${html5_framework}.sh
-done;
+echo "Initializing ${HTML5_FRAMEWORK} project."
+source bin/init-html5-${HTML5_FRAMEWORK}.sh
 
 # Initialize mobile targets
 for mobile_target in ${MOBILE_TARGETS}; do
