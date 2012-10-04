cb1621743e89c265c9ad792dda038678af3d70a8
Philippe <psamlong@laposte.net>
2012-10-04 17:30:12 +0200

    possibility to deploy an external project.

--- bin/deploy-cordova-android.sh
+++ bin/deploy-cordova-android.sh
@@ -7,7 +7,7 @@ source config/config.sh
 rm -rf ${ANDROID_PROJECT_DIR}/assets/www
 
 # Deploy enyo project into cordova
-(cd ${HTML5_PROJECT_DIR}/tools && TARGET=../../../${ANDROID_PROJECT_DIR}/assets/www bash deploy.sh)
+(cd ${HTML5_PROJECT_DIR}/tools && TARGET=${ENYO2CORDOVA_DIR}/${ANDROID_PROJECT_DIR}/assets/www bash deploy.sh)
 
 # Generate APK and install on device if connected
 (cd ${ANDROID_PROJECT_DIR} && cordova/debug)
--- config/config.sh
+++ config/config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Go to the root of enyo2cordova.
-cd $(dirname $(readlink -f $0))/..
+ENYO2CORDOVA_DIR=$(dirname $(readlink -f $0))/..
 
 source config/mobile-targets.conf
 source config/html5-project.conf
